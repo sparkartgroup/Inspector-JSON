fafe29b064f3405a0aaab9c066a99c7edac98759
Fauntleroy <the.kaempfer@gmail.com>
2013-10-28 17:50:45 -0700

    add ability to pass JSON on init

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ var test = require('tape');
 
 var TEST_MARKUP = ['<div id="json"></div>','<div id="json2"></div>'];
 var TEST_JSON = '{"string":"Hello World!","number":5,"boolean":true,"array":["one","two","three"],"object":{"key":"value","key2":"value2"}}';
+var TEST_JSON_MARKUP = '<ul><li class="string" data-path="this.string"><strong>string</strong><span>"Hello World!"</span></li><li class="number" data-path="this.number"><strong>number</strong><var>5</var></li><li class="boolean" data-path="this.boolean"><strong>boolean</strong><em>true</em></li><li class="array collapsed" data-path="this.array"><a href="#toggle"><strong>array</strong></a>Array(3)<ol><li class="string" data-path="this.array[0]"><strong>0</strong><span>"one"</span></li><li class="string" data-path="this.array[1]"><strong>1</strong><span>"two"</span></li><li class="string" data-path="this.array[2]"><strong>2</strong><span>"three"</span></li></ol></li><li class="object collapsed" data-path="this.object"><a href="#toggle"><strong>object</strong></a><ul><li class="string" data-path="this.object.key"><strong>key</strong><span>"value"</span></li><li class="string" data-path="this.object.key2"><strong>key2</strong><span>"value2"</span></li></ul></li></ul>';
 
 // add a test container element
 $('html').append('<div id="test"></div>');
@@ -31,12 +32,11 @@ test( 'Inspector JSON initializes and destroys itself', function( t ){
 test( 'Inspector JSON displays JSON for viewing', function( t ){
 	t.plan(7);
 	$('#test').html( TEST_MARKUP[0] );
-	var expected_markup = '<ul><li class="string" data-path="this.string"><strong>string</strong><span>"Hello World!"</span></li><li class="number" data-path="this.number"><strong>number</strong><var>5</var></li><li class="boolean" data-path="this.boolean"><strong>boolean</strong><em>true</em></li><li class="array collapsed" data-path="this.array"><a href="#toggle"><strong>array</strong></a>Array(3)<ol><li class="string" data-path="this.array[0]"><strong>0</strong><span>"one"</span></li><li class="string" data-path="this.array[1]"><strong>1</strong><span>"two"</span></li><li class="string" data-path="this.array[2]"><strong>2</strong><span>"three"</span></li></ol></li><li class="object collapsed" data-path="this.object"><a href="#toggle"><strong>object</strong></a><ul><li class="string" data-path="this.object.key"><strong>key</strong><span>"value"</span></li><li class="string" data-path="this.object.key2"><strong>key2</strong><span>"value2"</span></li></ul></li></ul>';
 	var inspector = new InspectorJSON({
 		element: 'json'
 	});
 	inspector.view( TEST_JSON );
-	t.ok( $('#json').html() === expected_markup, 'Draws correct markup' );
+	t.ok( $('#json').html() === TEST_JSON_MARKUP, 'Draws correct markup' );
 	var $array = $('#test li[data-path="this.array"]');
 	t.ok( $array.is('.collapsed'), 'this.array group is collapsed' );
 	$array.children('a')[0].click();
@@ -65,6 +65,18 @@ test( 'InspectorJSON can start uncollapsed', function( t ){
 	t.ok( !$array.is('.collapsed'), 'this.array group is not collapsed' );
 	var $object = $('#test li[data-path="this.object"]');
 	t.ok( !$object.is('.collapsed'), 'this.object group is not collapsed' );
+	inspector.destroy();
+	$('#test').html('');
+});
+
+test( 'InspectorJSON automatically renders JSON when provided', function( t ){
+	t.plan(1);
+	$('#test').html( TEST_MARKUP[0] );
+	var inspector = new InspectorJSON({
+		element: 'json',
+		json: TEST_JSON
+	});
+	t.ok( $('#json').html() === TEST_JSON_MARKUP, 'Draws markup automatically' );
 	inspector.destroy();
 	$('#test').html('');
 });
